ab2783dd60c869e2149370e9d5681bc0f3e9a1e2
minhajul-im <minhajul.cpp@gmail.com>
2023-07-11 17:55:51 +0600

    function timer done

--- Module-5/src/layout/Page.jsx
+++ Module-5/src/layout/Page.jsx
@@ -1,28 +1,60 @@
 import classes from "./Page.module.css";
+import { useState, useEffect } from "react";
 
 const Page = ({ data }) => {
-  const { title, clock, mainTitle, button, text } = data;
+  const { title, mainTitle, button } = data;
+
+  // time function
+  const [clock, setClock] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setClock(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  // input value
+  const [text, setText] = useState("");
+  const handelTextarea = (e) => {
+    setText(e.target.value);
+  };
+
+  // button function
+  const handelButton = () => {
+    setText("");
+  };
 
   return (
     <div>
       <header className={classes.header}>
         <h1 className={classes.title}>{title}</h1>
       </header>
+
       <main className={classes.main}>
-        <h3 className={classes.clock}>{clock}</h3>
+        <h3 className={classes.clock}>{clock.toLocaleTimeString()}</h3>
+
         <h1 className={classes.mainHeader}>{mainTitle}</h1>
+
         <section className={classes.section}>
           <textarea
             className={classes.textarea}
-            name=""
-            id=""
+            onChange={handelTextarea}
+            value={text}
             cols="30"
             rows="5"
           ></textarea>
-          <button className={classes.button}>{button}</button>
-          <p className={classes.text}></p>
+
+          <button onClick={handelButton} className={classes.button}>
+            {button}
+          </button>
+
+          <p className={classes.text}>{text}</p>
         </section>
       </main>
+
       <footer></footer>
     </div>
   );
