d086e08149326e34ecb26a0c53f743a1477e3610
minhajul-im <minhajul.cpp@gmail.com>
2023-07-18 22:44:57 +0600

    live link

---
+++ Module-6/LIVELINK.md
@@ -0,0 +1,3 @@
+# please => alt + click then open the new tab
+
+# https://hey-todo-app.netlify.app/
--- Module-6/src/function/DeleteButton.jsx
+++ Module-6/src/function/DeleteButton.jsx
@@ -1,4 +1,5 @@
 export const handleDeleteTask = (index, allTask, setAllTask) => {
   const updatedTasks = allTask.filter((_item, key) => key !== index);
+  alert(`Are you sure? Do you wanna delete your task?`);
   setAllTask(updatedTasks);
 };
