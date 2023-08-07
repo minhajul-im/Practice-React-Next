6c8ae1044bd0425766257d11f5043e1579e45ccb
minhajul-im <minhajul.cpp@gmail.com>
2023-08-07 19:48:34 +0600

    Module-8 Started Now

--- Module-8/src/app/layout.js
+++ Module-8/src/app/layout.js
@@ -1,6 +1,6 @@
+import Nav from "@/components/nav/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Nav from "./navbar/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={inter.className}>
         <Nav />
         {children}
       </body>
---
+++ Module-8/src/components/nav/Nav.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <div>
+      <p>component</p>
+    </div>
+  );
+};
+
+export default Nav;
