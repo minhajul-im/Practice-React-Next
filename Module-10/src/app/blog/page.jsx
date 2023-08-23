8c726833bed6d22bd192420d8e147ba40613ba30
minhajul-im <minhajul.cpp@gmail.com>
2023-08-23 15:16:20 +0600

    some code reusable

--- Module-10/src/app/blog/page.jsx
+++ Module-10/src/app/blog/page.jsx
@@ -1,30 +1,9 @@
-import fetchData from "@/utils/fetchData";
-import Image from "next/image";
-import Link from "next/link";
+import Card from "@/components/Card";
 
-const Page = async () => {
-  const data = await fetchData("post-newest");
-
+const Page = () => {
   return (
-    <div className="grid grid-cols-3 gap-4 my-4">
-      {data.map((item, key) => (
-        <Link
-          href={`/blog/single-blog/${item?.id}`}
-          key={key}
-          className="border border-gray-100 
-        rounded-md p-2"
-        >
-          <Image
-            className="mx-auto rounded-md"
-            src={item?.img}
-            alt="tamim"
-            height={200}
-            width={300}
-          />
-          <h1 className="text-2xl my-2 whitespace-pre-line">{item?.title}</h1>
-          <p>{item?.short}</p>
-        </Link>
-      ))}
+    <div>
+      <Card />
     </div>
   );
 };
--- Module-10/src/components/Card.jsx
+++ Module-10/src/components/Card.jsx
@@ -1,27 +1,29 @@
-"use client";
-
+import fetchData from "@/utils/fetchData";
 import Image from "next/image";
 import Link from "next/link";
 
-const Card = async ({ data }) => {
+const Card = async () => {
+  const data = await fetchData("post-newest");
+
   return (
     <div className="grid grid-cols-3 gap-4 my-4">
       {data.map((item, key) => (
-        <div
+        <Link
+          href={`/blog/single-blog/${item?.id}`}
           key={key}
           className="border border-gray-100 
         rounded-md p-2"
         >
           <Image
             className="mx-auto rounded-md"
-            src={item.img}
+            src={item?.img}
             alt="tamim"
             height={200}
             width={300}
           />
           <h1 className="text-2xl my-2 whitespace-pre-line">{item?.title}</h1>
           <p>{item?.short}</p>
-        </div>
+        </Link>
       ))}
     </div>
   );
