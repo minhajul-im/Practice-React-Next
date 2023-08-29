1584369f32288d9d7bdf36dc84abb30ac869906f
minhajul-im <minhajul.cpp@gmail.com>
2023-08-29 20:05:20 +0600

    all task done

--- Module-11/src/app/page.jsx
+++ Module-11/src/app/page.jsx
@@ -1,11 +1,13 @@
 import Set_Cookies from "@/components/Set_Cookies";
 import Show_Display from "@/components/Show_Display";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="w-4/6 mx-auto">
       <Set_Cookies />
       <Show_Display />
     </div>
   );
-}
+};
+
+export default Home;
