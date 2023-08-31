6b865dea64d9ba12741e5ad5953675ff15ad9c65
minhajul-im <minhajul.cpp@gmail.com>
2023-08-31 11:04:54 +0600

    module-12 project structure

---
+++ Module-12/src/app/page.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-red-600 text-3xl font-mono font-bold p-10">
+        Hello Home Page
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
