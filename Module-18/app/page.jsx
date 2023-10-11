c64bd5e8848a68efb1cd97f003ddb56af732c492
minhajul-im <minhajul.cpp@gmail.com>
2023-10-11 21:45:26 +0600

    module-18 done

--- Module-18/app/page.jsx
+++ Module-18/app/page.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-
-export default function Home() {
+const Home = () => {
   return (
     <main>
       <h1> Hello Prisma</h1>
-   </main>
-  )
-}
+    </main>
+  );
+};
+
+export default Home;
