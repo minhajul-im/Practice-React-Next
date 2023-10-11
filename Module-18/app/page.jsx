c113c7e5aff829485360d56e5c304cdb06fff55e
minhajul-im <minhajul.cpp@gmail.com>
2023-10-11 17:20:50 +0600

    module-18 blog schema done

--- Module-18/app/page.jsx
+++ Module-18/app/page.jsx
@@ -1,8 +1,9 @@
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello Prisma</h1>
-    </main>
+      <h1> Hello Prisma</h1>
+   </main>
   )
 }
