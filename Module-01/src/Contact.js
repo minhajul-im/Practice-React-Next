5a285159d1c72038cb918cf6d44f0bbe75dc93d1
minhajul-im <minhajul.cpp@gmail.com>
2023-10-22 16:31:16 +0600

    module-01 added some file in src

---
+++ Module-01/src/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <section>
+      <h1>This is a contact page!</h1>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ Module-01/src/Home.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Product from "./components/Product";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>আমাদের নতুন নতুন পণ্যসমূহ!</h1>
+      <Product />
+    </div>
+  );
+};
+export default Home;
