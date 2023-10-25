1746c474a16ce943ef552adaf93323ee292e1214
minhajul-im <minhajul.cpp@gmail.com>
2023-10-25 17:48:32 +0600

    updated module-19

--- Module-19/src/app/page.jsx
+++ Module-19/src/app/page.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import {
+  CustomElementRenderer,
+  UserLoginFormConfig,
+} from "@/components/CustomRenderElement";
 
 const Home = () => {
+  const [state, setState] = useState({});
+  const [formData, setFormData] = useState({});
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setFormData(state);
+  };
+
+  const handleOnChange = (name, value) => {
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   return (
-    <main>
-      <h1>Hello Module-19</h1>
-    </main>
+    <>
+      <form onSubmit={handleFormSubmit}>
+        {UserLoginFormConfig.map((obj) => {
+          return (
+            <CustomElementRenderer obj={obj} onChangeFunc={handleOnChange} />
+          );
+        })}
+      </form>
+      <span>
+        <h3>Data submitted: </h3>
+        {JSON.stringify(formData)}
+      </span>
+    </>
   );
 };
 
