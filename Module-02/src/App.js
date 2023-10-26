643551bca8cfc0ad33444b129d346ee3cf64f731
minhajul-im <minhajul.cpp@gmail.com>
2023-10-26 18:02:12 +0600

    started modeul-02 only core react tailwind css

---
+++ Module-02/src/App.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hey There, How's it going?</h1>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
