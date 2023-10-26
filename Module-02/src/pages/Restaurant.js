8bdceef77d1458503c6b8a08f6223a1358fe113e
minhajul-im <minhajul.cpp@gmail.com>
2023-10-26 22:03:33 +0600

    swiggy app product done

--- Module-02/src/pages/Restaurant.js
+++ Module-02/src/pages/Restaurant.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { SWIGGY_API, SWIGGY_IMG } from "../utils/allLinks";
 
 const Restaurant = () => {
+  const [resData, setResData] = useState([]);
+
+  useEffect(() => {
+    fetchApiSwiggy();
+  }, []);
+
+  const fetchApiSwiggy = async () => {
+    const response = await fetch(SWIGGY_API);
+    const json = await response.json();
+    const restaurants =
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setResData(restaurants);
+  };
+
   return (
-    <div className="bg-purple-300">
-      <h1 className="text-6xl text-red-700">Hey there, How's it going?</h1>
-    </div>
+    <main className="w-5/6 mx-auto mt-10">
+      <div className="flex flex-wrap gap-8 justify-center">
+        {resData.slice(0, 8).map((item) => (
+          <div
+            key={item?.info?.id}
+            className="rounded-2xl w-[331px] hover:shadow-md"
+          >
+            <img
+              className="h-[220px] w-[330px] rounded-lg"
+              src={SWIGGY_IMG + item?.info?.cloudinaryImageId}
+              alt="x"
+            />
+            <div className="py-4 flex flex-col gap-2">
+              <h5 className="text-[18px] font-bold ">
+                {item?.info?.costForTwo}
+              </h5>
+              <h5 className="text-[18px] font-semibold ">{item?.info?.name}</h5>
+              <div className="font-semibold text-[16px] flex gap-2 flex-row">
+                ⭐<span> {item?.info?.avgRating}</span>•
+                <span> {item?.info?.sla?.slaString} </span>
+              </div>
+              <p className="text-gray-500">{item?.info?.cuisines.join(", ")}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 };
 
--- Module-02/src/utils/allLinks.js
+++ Module-02/src/utils/allLinks.js
@@ -3,3 +3,6 @@ export const SWIGGY_LOGO =
 
 export const SWIGGY_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const SWIGGY_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
