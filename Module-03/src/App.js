375364d40ee9ff5fbdb40547549d5282bb4ab008
minhajul-im <minhajul.cpp@gmail.com>
2023-10-28 05:59:10 +0600

    module-02 react install done

---
+++ Module-03/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
