3ef83ea917b6f66449e1c80a878feeba9dde729e
minhajul-im <minhajul.cpp@gmail.com>
2023-11-03 11:19:02 +0600

    started modele-05

---
+++ Module-05/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-center text-red-600 text-2xl">hello Module-05</h1>
+    </main>
+  );
+};
+
+export default App;
--- Module-5/LIVELINK.md
+++
@@ -1,3 +0,0 @@
-# please ==> alt + click then open the new tab
-
-# https://the-clock-react.netlify.app/
