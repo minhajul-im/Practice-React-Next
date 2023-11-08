647ad35703437094f3caefa45ee9a0f724b205de
minhajul-im <minhajul.cpp@gmail.com>
2023-11-08 19:53:22 +0600

    create reducer

---
+++ Module-07/src/reducer/taskReducer.js
@@ -0,0 +1,26 @@
+export const initial = {
+  tasks: [
+    {
+      id: crypto.randomUUID(),
+      title: "Create Tasker Application!",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil illum aspernatur molestiae, placeat fugiat ntur adipisicing elit.",
+      tags: ["JavaScript", "python", "java"],
+      priority: "High",
+      isFovarite: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Tasker Application!",
+      description:
+        "Lorem, ipsum ctetur adipisicing elit. Nihil illum aspernatur molestiae, placeat fugiat nullaipsum ctetur adipisicing elit.",
+      tags: ["JavaScript", "python", "java"],
+      priority: "Low",
+      isFovarite: false,
+    },
+  ],
+  show: false,
+  editTask: null,
+};
+
+export const taskReducer = (state, action) => {};
