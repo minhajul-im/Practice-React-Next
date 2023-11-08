ef824a834eb7c4ba77e1085c36b5c4e4680be99e
minhajul-im <minhajul.cpp@gmail.com>
2023-11-08 19:54:04 +0600

    create add task reducer

--- Module-07/src/reducer/taskReducer.js
+++ Module-07/src/reducer/taskReducer.js
@@ -23,4 +23,29 @@ export const initial = {
   editTask: null,
 };
 
-export const taskReducer = (state, action) => {};
+export const taskReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TASK": {
+      if (!state.editTask) {
+        return {
+          ...state,
+          tasks: [...state.tasks, { ...action.task, id: crypto.randomUUID() }],
+        };
+      } else {
+        return {
+          ...state,
+          tasks: state.tasks.map((task) => {
+            if (task.id === action.task.id) {
+              return action.task;
+            } else {
+              return task;
+            }
+          }),
+        };
+      }
+    }
+
+    default:
+      throw Error(`It's an invalid ${action.type}!`);
+  }
+};
