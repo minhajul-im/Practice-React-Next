7b30113f2a015d3fb9cfcf1ab8209f84b84706b9
minhajul-im <minhajul.cpp@gmail.com>
2023-11-09 13:50:25 +0600

    started module-06

--- Module-06/src/App.jsx
+++ Module-06/src/App.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import { createBrowserRouter, Outlet } from "react-router-dom";
-import About from "./pages/About";
+
+import Food from "./pages/Food";
 import Body from "./pages/Body";
+import Task from "./pages/Task";
 import Header from "./components/Header";
+import ContextData from "./utils/ContextData";
 
 const App = () => {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const data = {
+      homeName: "Minhaj Muhammad",
+      foodPage: "Hey there, Here all amazing Food😋",
+      navbarName: "Hey, I'm Minhaj!",
+      taskName: "Hey, you can create your task!",
+    };
+    setState(data.homeName);
+  }, []);
+
   return (
     <main>
-      <Header />
-      <Outlet />
+      <ContextData.Provider value={{ username: state, setState }}>
+        <Header />
+        <Outlet />
+      </ContextData.Provider>
     </main>
   );
 };
@@ -21,8 +39,12 @@ const AppRouter = createBrowserRouter([
         element: <Body />,
       },
       {
-        path: "/about",
-        element: <About />,
+        path: "/task",
+        element: <Task />,
+      },
+      {
+        path: "/food",
+        element: <Food />,
       },
     ],
   },
