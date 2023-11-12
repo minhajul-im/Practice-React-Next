aa0eeead283428c669438257338e2f17b41bc5fe
minhajul-im <minhajul.cpp@gmail.com>
2023-11-12 13:52:47 +0600

    utils function added

---
+++ Module-06/src/utils/ContextData.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+const ContextData = createContext({
+  username: "Default_Context_Data: Minhajul Islam (Minhaj)",
+  food: "burger",
+  task: "task",
+});
+
+export default ContextData;
