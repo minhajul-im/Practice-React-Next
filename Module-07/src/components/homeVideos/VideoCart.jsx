1f29938aa31151fbec11e53520635051d59a3df5
minhajul-im <minhajul.cpp@gmail.com>
2023-11-20 21:40:03 +0600

    link added every video

--- Module-07/src/components/homeVideos/VideoCart.jsx
+++ Module-07/src/components/homeVideos/VideoCart.jsx
@@ -1,46 +1,42 @@
 import { Link } from "react-router-dom";
 
-export default function VideoCart() {
+export default function VideoCart({ video }) {
+  const { author, avatar, title, duration, views, thumbnail, date, id } = video;
+
   return (
     <section className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div className="w-full flex flex-col">
         <div className="relative">
-          <Link to={"/watch/1"}>
+          <Link to={`/watch/${id}`}>
             <img
-              src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
+              src={thumbnail}
               className="w-full h-auto"
               alt="Some video title"
             />
           </Link>
 
           <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-            12:10
+            {duration}
           </p>
         </div>
 
         <div className="flex flex-row mt-2 gap-2">
-          <Link to={"/watch/1"} className="shrink-0">
-            <img
-              src="https://avatars.githubusercontent.com/u/73503432?v=4"
-              className="rounded-full h-6 w-6"
-              alt="Learn with Sumit"
-            />
+          <Link to={`/watch/${id}`} className="shrink-0">
+            <img src={avatar} className="rounded-full h-6 w-6" alt={author} />
           </Link>
 
           <div className="flex flex-col">
-            <Link to={"/watch/1"}>
-              <p className="text-slate-900 text-sm font-semibold">
-                Video title
-              </p>
+            <Link to={`/watch/${id}`}>
+              <p className="text-slate-900 text-sm font-semibold">{title}</p>
             </Link>
             <Link
-              to={"/watch/1"}
-              className="text-gray-400 text-xs mt-2 hover:text-gray-600"
+              to={`/watch/${id}`}
+              className="text-gray-500 text-xs mt-2 hover:text-gray-600"
             >
-              Learn with Sumit
+              {author}
             </Link>
-            <p className="text-gray-400 text-xs mt-1">
-              200 views . May 3, 2022
+            <p className="text-gray-500 text-xs mt-1">
+              {views} views . {date}
             </p>
           </div>
         </div>
