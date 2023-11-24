b730598e59e2458d7f867d83026d94eef8fc06a4
minhajul-im <minhajul.cpp@gmail.com>
2023-11-24 18:35:53 +0600

    module-08 started

---
+++ Module-08/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-2xl text-red-600 font-semibold text-center mt-10">
+      App
+    </div>
+  );
+}
--- Module-8/src/app/page.jsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div>Hello Home page </div>;
-};
-
-export default Page;
