0b8d7ed4ea402cc57a53c7f7b083160ca7314797
minhajul-im <minhajul.cpp@gmail.com>
2023-11-30 13:02:36 +0600

    modeule-02 netflix project

---
+++ Module-02/src/index.js
@@ -0,0 +1,6 @@
+import "./index.css";
+import App from "./app/App";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ Module-02/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center font-bold py-4 text-purple-600">
+        Hey React Developer!
+      </h1>
+    </div>
+  );
+}
