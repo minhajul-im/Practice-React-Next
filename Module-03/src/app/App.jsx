27296c7515afea6dfe6bcb905acd6c6da2954dee
minhajul-im <minhajul.cpp@gmail.com>
2023-12-01 19:45:34 +0600

    stated module-03 with pnpm

---
+++ Module-03/src/app/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-red-600 font-bold text-center text-2xl">hello</h1>
+      <p className=" text-center text-green-600 font-bold">Hey there! how is it going?</p>
+    </div>
+  );
+}
