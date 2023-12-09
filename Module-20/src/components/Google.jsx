b9ace388d39e283ddf44c06b8009c11845025c14
minhajul-im <minhajul.cpp@gmail.com>
2023-12-09 09:14:27 +0600

    github loging

--- Module-20/src/components/Google.jsx
+++ Module-20/src/components/Google.jsx
@@ -1,77 +1,47 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import Github from "./Github";
+
 export default function Google() {
+  const provider = new GoogleAuthProvider();
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  const handleSignUpByGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        const user = result.user;
+        console.log(token, "|", user);
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        const email = error.customData.email;
+
+        const credential = GoogleAuthProvider.credentialFromError(error);
+
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
   return (
     <div className="flex justify-center items-center">
       <div className="bg-white p-8 shadow-md rounded-md flex flex-col items-center">
         <h1 className="text-2xl font-bold mb-4">Login with Google & GitHub</h1>
-        <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md mb-4">
+        <button
+          onClick={handleSignUpByGoogle}
+          className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md mb-4"
+        >
           Login with Google
         </button>
-        <button className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-md">
-          Login with GitHub
-        </button>
+        <Github />
       </div>
     </div>
   );
 }
 // https://module-20-329fc.firebaseapp.com/__/auth/handler
-
-import { createContext, useContext, useState } from "react";
-
-const CurrentUserContext = createContext(null);
-
-export function MyApp() {
-  const [currentUser, setCurrentUser] = useState(null);
-  return (
-    <CurrentUserContext.Provider
-      value={{
-        currentUser,
-        setCurrentUser,
-      }}
-    >
-      <Form />
-    </CurrentUserContext.Provider>
-  );
-}
-
-function Form({ children }) {
-  return (
-    <Panel title="Welcome">
-      <LoginButton />
-    </Panel>
-  );
-}
-
-function LoginButton() {
-  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-
-  if (currentUser !== null) {
-    return <p>You logged in as {currentUser.name}.</p>;
-  }
-
-  return (
-    <Button
-      onClick={() => {
-        setCurrentUser({ name: "Advika" });
-      }}
-    >
-      Log in as Advika
-    </Button>
-  );
-}
-
-function Panel({ title, children }) {
-  return (
-    <section className="panel">
-      <h1>{title}</h1>
-      {children}
-    </section>
-  );
-}
-
-function Button({ children, onClick }) {
-  return (
-    <button className="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-}
