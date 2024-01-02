5b9ed117901185fe3ad054e2402cb06b5adf4094
minhajul-im <minhajul.cpp@gmail.com>
2024-01-02 14:40:22 +0600

    navbar added finder app

---
+++ Module-02/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { FaRegBell } from "react-icons/fa";
+
+import logoLWS from "../assets/lws-logo-en.svg";
+
+export default function Navbar() {
+  return (
+    <nav className='py-6 '>
+      <div className='container mx-auto flex items-center justify-between gap-x-6 max-w-7xl'>
+        <a href='/'>
+          <img
+            className='max-w-[100px] md:max-w-[165px]'
+            src={logoLWS}
+            alt='Lws'
+          />
+        </a>
+
+        <FaRegBell />
+      </div>
+    </nav>
+  );
+}
