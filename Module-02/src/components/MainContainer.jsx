9e5f50f90ed6796cc51f41412f4de6fa12f738ae
minhajul-im <minhajul.cpp@gmail.com>
2024-01-02 14:43:29 +0600

    bookslist  added

--- Module-02/src/components/MainContainer.jsx
+++ Module-02/src/components/MainContainer.jsx
@@ -1,8 +1,11 @@
+// import Header from "./Header";
+import BooksList from "./BooksList";
+
 export default function MainContainer() {
   return (
     <div className='my-10 lg:my-14'>
       {/* <Header /> */}
-      {/* <BooksList /> */}
+      <BooksList />
     </div>
   );
 }
