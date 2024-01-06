f67218650cb1642fe07dc58a4e8f5a126e9be438
minhajul-im <minhajul.cpp@gmail.com>
2024-01-06 12:37:42 +0600

    calculate winner funtion

--- Module-01/src/components/Board.jsx
+++ Module-01/src/components/Board.jsx
@@ -1,29 +1,11 @@
 import Square from "./Square";
-
-const calculateWinner = (squares) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [x, y, z] = lines[i];
-
-    if (squares[x] && squares[x] === squares[y] && squares[x] === squares[z]) {
-      return squares[x];
-    }
-  }
-  return null;
-};
+import { calculateWinner } from "../utilities/calculateWinner";
 
 export default function Board({ squares, isNext, onPlayGame }) {
   const winner = calculateWinner(squares);
+
   let status;
+
   if (winner) {
     status = `Winner: ${winner}`;
   } else {
