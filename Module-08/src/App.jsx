614cf98edd69366d24f48c03abfaa9fc8f97c635
minhajul-im <minhajul.cpp@gmail.com>
2024-01-06 21:34:25 +0600

    start use Ref project

---
+++ Module-08/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div className='text-red-600 font-bold text-center text-2xl'>App</div>;
+}
---
+++ Module-08/src/main.jsx
@@ -0,0 +1,5 @@
+import "./style.css";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
