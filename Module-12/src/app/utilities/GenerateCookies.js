263035ee1919faa9d53c7f94298f081c52b1c0f3
minhajul-im <minhajul.cpp@gmail.com>
2024-01-08 12:22:46 +0600

    update module-12

--- Module-12/src/app/utilities/GenerateCookies.js
+++
@@ -1,9 +0,0 @@
-import { SignInJwtFunc } from "./JwtHelper";
-
-export const generateCookiesFunc = async (email) => {
-  const token = await SignInJwtFunc(email);
-
-  return {
-    "Set-Cookie": `token=${token}; Max-Age=7200; Source=httpOnly; Path=/; SameSite=Strict`,
-  };
-};
