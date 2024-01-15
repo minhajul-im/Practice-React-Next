aabf9683d2d0df918fb8026511caebb3f4b4f483
minhajul-im <minhajul.cpp@gmail.com>
2024-01-15 12:40:03 +0600

    update module-15

--- Module-15/src/components/GetSessionProvider.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
-const GetSessionProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default GetSessionProvider;
