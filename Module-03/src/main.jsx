558cd20ff096a837b948511cbf21283166336b4f
minhajul-im <minhajul.cpp@gmail.com>
2024-01-18 08:46:11 +0600

    module-03 update

--- Module-03/src/main.jsx
+++ Module-03/src/main.jsx
@@ -1,6 +1,5 @@
 import "./index.css";
-
-import ReactDOM from "react-dom/client";
 import App from "./app/App";
+import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- Module-03/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Form from "../components/Form";
-
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Form />
-    </div>
-  );
-}
