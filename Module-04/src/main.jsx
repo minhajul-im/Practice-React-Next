f95bcc210690d3e6a66309177304bd88a7b7d9c4
minhajul-im <minhajul.cpp@gmail.com>
2024-01-24 09:16:33 +0600

    update module - 04

--- Module-04/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import "./index.css";
-import RouterApp from "./App";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={RouterApp} />
-);
--- Module-04/src/pages/Contact.jsx
+++
@@ -1,5 +0,0 @@
-const Contact = () => {
-  return <div>Contact page</div>;
-};
-
-export default Contact;
