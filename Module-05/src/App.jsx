a68c93ac5e7fdac1925fb71e9f442c096d69b9c3
minhajul-im <minhajul.cpp@gmail.com>
2024-01-26 15:30:39 +0600

    update module - 05

--- Module-05/src/App.jsx
+++ Module-05/src/App.jsx
@@ -1,44 +1,5 @@
-import { Provider } from "react-redux";
-import { createBrowserRouter, Outlet } from "react-router-dom";
+import React from "react";
 
-import Cart from "./pages/Cart";
-import Menu from "./pages/Menu";
-import appStore from "./redux/appStore";
-import ErrorPage from "./pages/ErrorPage";
-import Restaurants from "./pages/Restaurants";
-import Header from "./components/header/Header";
-
-const App = () => {
-  return (
-    <Provider store={appStore}>
-      <Header />
-      <section className="w-5/6 mx-auto">
-        <Outlet />
-      </section>
-    </Provider>
-  );
-};
-
-const RouterApp = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Menu />,
-      },
-      {
-        path: "/restaurants/:id",
-        element: <Restaurants />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-    ],
-    errorElement: <ErrorPage />,
-  },
-]);
-
-export default RouterApp;
+export default function App() {
+  return <div>App</div>;
+}
--- Module-05/src/main.jsx
+++ Module-05/src/main.jsx
@@ -1,8 +1,10 @@
 import "./index.css";
-import RouterApp from "./App";
+import App from "./App";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={RouterApp} />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
