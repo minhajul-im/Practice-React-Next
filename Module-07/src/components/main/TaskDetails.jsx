adfd5b0fea30a46730e005b3c3f20758869eca36
minhajul-im <minhajul.cpp@gmail.com>
2024-01-28 19:47:38 +0600

    added task details component

---
+++ Module-07/src/components/main/TaskDetails.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function TaskDetails() {
+  return (
+    <thead>
+      <tr>
+        <th className='p-4 pb-8 text-sm font-semibold capitalize w-[48px]'></th>
+        <th className='p-4 pb-8 text-sm font-semibold capitalize w-[300px]'>
+          Title
+        </th>
+        <th className='p-4 pb-8 text-sm font-semibold capitalize w-full'>
+          Description
+        </th>
+        <th className='p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]'>
+          Tags
+        </th>
+        <th className='p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]'>
+          Priority
+        </th>
+        <th className='p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]'>
+          Options
+        </th>
+      </tr>
+    </thead>
+  );
+}
