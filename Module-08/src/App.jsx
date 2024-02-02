7894cb34e2b8ab6436216b8100a2e4bf0869608a
minhajul-im <minhajul.cpp@gmail.com>
2024-02-02 10:43:32 +0600

    use effect use ref

--- Module-08/src/App.jsx
+++ Module-08/src/App.jsx
@@ -1,31 +1,9 @@
-// import { useState } from "react";
-
-// import Closer from "./components/Closer";
-// import Fetching from "./components/Fetching";
-import Parent from "./components/Parent";
+import Video from "./effeccts/Video";
 
 export default function App() {
-  // const [show, setShow] = useState(false);
   return (
     <div>
-      {/* <h1>Application!</h1>
-
-      <button onClick={() => setShow(!show)}>
-        {show ? "Unmount" : "Mount"}
-      </button>
-
-      <br />
-      {show && <hr />}
-      {show && <Closer />} */}
-
-      {/* <div>
-        <hr />
-        <h1>Hello Data</h1>
-        <Fetching postId={"1"} />
-      </div> */}
-
-      <hr />
-      <Parent />
+      <Video />
     </div>
   );
 }
