dd4d4db0ccfba0214e5486719f0ecdfa7073ab62
minhajul-im <minhajul.cpp@gmail.com>
2024-02-05 23:11:34 +0600

    added video card

---
+++ Module-09/src/components/VideoCard.jsx
@@ -0,0 +1,34 @@
+import { getTimeAgo } from "../utilities/date";
+
+import { images } from "../db/navbarData";
+
+export default function VideoCard({ videoInfo }) {
+  return (
+    <div className='w-[360px] h-[310px] rounded-lg hover:scale-100'>
+      <img
+        className='w-full object-cover rounded-lg'
+        src={videoInfo?.snippet?.thumbnails?.medium?.url}
+        alt={videoInfo?.snippet.title.slice(0, 30)}
+      />
+      <div className='flex gap-3 py-2 mt-2'>
+        <img
+          className='img w-10 h-10 rounded-full cursor-pointer border border-green-600'
+          src={images.user}
+          alt={videoInfo?.snippet?.channelTitle}
+        />
+        <ul className=' text-gray-500'>
+          <li className='font-medium text-black'>
+            {videoInfo?.snippet.title.slice(0, 60)}...
+          </li>
+          <li className='font-semibold text-sm'>
+            {videoInfo?.snippet.channelTitle}
+          </li>
+          <li className='text-sm'>
+            {(videoInfo?.statistics?.commentCount / 1000).toFixed(2)}k •{" "}
+            {getTimeAgo(videoInfo?.snippet?.publishedAt)}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
