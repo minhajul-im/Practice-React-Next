eeabdf68f609bed8e3e88cb0f7b82bd0bb3be30f
minhajul-im <minhajul.cpp@gmail.com>
2024-02-06 15:46:32 +0600

    update navar and footer

--- Module-07/src/components/footer/Footer.jsx
+++ Module-07/src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className='py-6 md:py-8'>
       <div className='container mx-auto'>
-        <p className='text-center text-xs text-[#6A6A6A] lg:text-sm'>
-          Copyright ©2024 | All rights reserved by Learn with Sumit
+        <p className='text-center text-xs text-white lg:text-sm italic'>
+          Copyright ©2024 minhaj
         </p>
       </div>
     </footer>
--- Module-07/src/components/header/Navbar.jsx
+++ Module-07/src/components/header/Navbar.jsx
@@ -1,11 +1,15 @@
-import Logo from "../../assets/lws-logo-en.svg";
-
 export default function Navbar() {
   return (
     <nav className='py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-10'>
       <div className='container mx-auto flex items-center justify-between gap-x-6'>
         <a href='/'>
-          <img className='h-[45px]' src={Logo} alt='Logo' />
+          <img
+            className='h-16 w-16 rounded-full border-2 border-green-600'
+            src={
+              "https://www.minhajul.com/_next/image?url=%2Fminhajul.png&w=256&q=75"
+            }
+            alt='Logo'
+          />
         </a>
       </div>
     </nav>
