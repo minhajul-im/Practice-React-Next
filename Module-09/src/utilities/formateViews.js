fcb1491c0d1d72129ca6fa20ec2d618a55002b76
minhajul-im <minhajul.cpp@gmail.com>
2024-02-07 15:09:54 +0600

    added aside context

--- Module-09/src/utilities/formateViews.js
+++
@@ -1,9 +0,0 @@
-export function formateViews(number) {
-  if (number >= 1000000) {
-    return (number / 1000000).toFixed(1) + "M";
-  } else if (number >= 1000) {
-    return (number / 1000).toFixed(1) + "K";
-  } else {
-    return number.toString();
-  }
-}
