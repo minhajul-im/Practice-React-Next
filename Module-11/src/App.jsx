07fca766984e2bc905338546ff13b35a01ee0236
minhajul-im <minhajul.cpp@gmail.com>
2024-02-09 20:25:50 +0600

    start newwfeed application

---
+++ Module-11/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NewsFeed from "./page/NewsFeed";
+
+export default function App() {
+  return (
+    <div>
+      <NewsFeed />
+    </div>
+  );
+}
---
+++ Module-11/src/page/NewsFeed.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NewsFeed() {
+  return <div>NewsFeed</div>;
+}
