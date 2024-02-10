8113833c46082fa029780e49be4d3addce3033cc
minhajul-im <minhajul.cpp@gmail.com>
2024-02-11 00:01:29 +0600

    added single popular news

---
+++ Module-11/src/components/news/PopularNewsItem.jsx
@@ -0,0 +1,20 @@
+import img2 from "../../assets/thumb_lg.png";
+export default function PopularNewsItem() {
+  return (
+    <div className='col-span-12 mb-6 md:col-span-8'>
+      <img className='w-full' src={img2} alt='thumb' />
+
+      <div className='col-span-12 mt-6 md:col-span-4'>
+        <a href='#'>
+          <h3 className='mb-2.5 text-xl font-bold lg:text-[20px]'>
+            Why is Uber selling its autonomous-vehicle division?
+          </h3>
+        </a>
+        <p className='text-base text-[#292219]'>
+          Self-driving cars were meant to be its future
+        </p>
+        <p className='mt-5 text-base text-[#94908C]'>25 Feb 2021</p>
+      </div>
+    </div>
+  );
+}
