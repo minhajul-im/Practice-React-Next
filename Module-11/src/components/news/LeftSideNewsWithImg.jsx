a0d0aa8f90420a78987e88e8ae1d3c0ba59581d8
minhajul-im <minhajul.cpp@gmail.com>
2024-02-11 18:52:56 +0600

    top left side news with image

---
+++ Module-11/src/components/news/LeftSideNewsWithImg.jsx
@@ -0,0 +1,26 @@
+import defaultImg from "../../assets/thumb.png";
+import { newsDateFormet } from "../../utilities/newsDateFormet";
+
+// eslint-disable-next-line react/prop-types
+export default function LeftSideNewsWithImg({ newsInfo }) {
+  const { content, description, publishedAt, title, url, urlToImage } =
+    newsInfo || {};
+
+  return (
+    <div className='col-span-12 grid grid-cols-12 gap-4 lg:col-span-8'>
+      <div className='col-span-12 md:col-span-6'>
+        <a href={url}>
+          <h3 className='mb-2.5 text-xl font-bold lg:text-2xl'>{title}</h3>
+        </a>
+        <p className='text-base text-[#292219]'>{description || content}</p>
+        <p className='mt-5 text-base text-[#5C5955]'>
+          {publishedAt && newsDateFormet(publishedAt)}
+        </p>
+      </div>
+
+      <div className='col-span-12 md:col-span-6'>
+        <img className='w-full' src={urlToImage || defaultImg} alt={title} />
+      </div>
+    </div>
+  );
+}
