a38dfe6a5c91adf1b4ff868cb87b526d0fb7fb59
minhajul-im <minhajul.cpp@gmail.com>
2024-02-11 18:54:33 +0600

    single right side news

---
+++ Module-11/src/components/news/SingleRightSideNews.jsx
@@ -0,0 +1,20 @@
+import { newsDateFormet } from "../../utilities";
+
+// eslint-disable-next-line react/prop-types
+export default function SingleRightSideNews({ newsInfo }) {
+  const { content, description, publishedAt, title, url } = newsInfo || {};
+
+  return (
+    <div className='col-span-12 md:col-span-8'>
+      <div className='col-span-12 md:col-span-4'>
+        <a href={url}>
+          <h3 className='mb-2.5 text-xl font-bold lg:text-[20px]'>{title}</h3>
+        </a>
+        <p className='text-base text-[#292219]'>{description || content}</p>
+        <p className='mt-5 text-base text-[#94908C]'>
+          {publishedAt && newsDateFormet(publishedAt)}
+        </p>
+      </div>
+    </div>
+  );
+}
