0723500d9723f38cb1f177eb6ffb95cd079e0769
minhajul-im <minhajul.cpp@gmail.com>
2024-02-14 19:03:40 +0600

    date functionality

---
+++ Module-11/src/components/header/Logo.jsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <a href='/'>
+      <img
+        className='max-w-[100px] md:max-w-[165px]'
+        src='./assets/logo.png'
+        alt='🚀🚀'
+      />
+    </a>
+  );
+}
