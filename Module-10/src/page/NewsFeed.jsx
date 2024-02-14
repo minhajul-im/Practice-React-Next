5cadf9888f4eca725d678125dc86673fff9324fe
minhajul-im <minhajul.cpp@gmail.com>
2024-02-14 08:37:35 +0600

    slightly changed

--- Module-10/src/page/NewsFeed.jsx
+++ Module-10/src/page/NewsFeed.jsx
@@ -2,12 +2,10 @@ import Header from "../components/header/Header";
 import LatestNews from "../components/news/LatestNews";
 import PopularNews from "../components/news/PopularNews";
 import Footer from "../components/footer/Footer";
-import LinkedInSearchBar from "../components/LinkedInSearchBar";
 
 export default function NewsFeed() {
   return (
     <>
-      {/* <LinkedInSearchBar /> */}
       <Header></Header>
       <main className='my-10 lg:my-14'>
         <div className='container mx-auto grid grid-cols-12 gap-8'>
