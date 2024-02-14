6dbe2552531d64d251ea7b21ce1d29b263dcb589
minhajul-im <minhajul.cpp@gmail.com>
2024-02-15 00:39:01 +0600

    slightly done dark & light mode

--- Module-11/src/provider/ThemeProvider.jsx
+++ Module-11/src/provider/ThemeProvider.jsx
@@ -1,12 +1,12 @@
-// import { useState } from "react";
-// import { ThemeContext } from "../context/ThemeContext";
+import { useState } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
-// // eslint-disable-next-line react/prop-types
-// export default function ThemeProvider({ children }) {
-//   const [darkMode, setDarkMode] = useState(true);
-//   return (
-//     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// }
+// eslint-disable-next-line react/prop-types
+export default function ThemeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
