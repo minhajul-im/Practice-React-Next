f0d2f7fceadeac366a248cf9eab55adc17d3ce35
minhajul-im <minhajul.cpp@gmail.com>
2024-02-15 21:31:49 +0600

    added tailwindcss

--- Module-12/src/App.tsx
+++ Module-12/src/App.tsx
@@ -1,6 +1,9 @@
-
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className='flex h-screen items-center justify-center'>
+      <h1 className='text-center text-3xl font-bold font-mono text-red-600'>
+        Hello TypeScript
+      </h1>
+    </div>
+  );
 }
