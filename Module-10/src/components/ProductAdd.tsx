4d3ad8495a7e1dd51cbf1a254b74284d3f285be0
minhajul-im <minhajul.cpp@gmail.com>
2024-02-23 00:19:20 +0600

    product add component

---
+++ Module-10/src/components/ProductAdd.tsx
@@ -0,0 +1,9 @@
+export default function ProductAdd() {
+  return (
+    <div className="bg-slate-300 w-3/12 flex flex-col items-center">
+      <h1 className="text-3xl font-bold text-center font-mono py-4 text-amber-600 underline">
+        Add Product
+      </h1>
+    </div>
+  );
+}
