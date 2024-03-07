aef058211eda324cf6b78389c83cc29a92502357
minhajul-im <minhajul.cpp@gmail.com>
2024-03-07 16:16:54 +0600

    context created

---
+++ Module-14/src/contexts/index.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthorContext = createContext();
+
+export const AuthorActionContext = createContext();
