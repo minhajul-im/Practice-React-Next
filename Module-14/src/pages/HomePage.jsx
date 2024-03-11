f2f8a0b5b9197f87d25f06cfc76cf43bbacc79a6
minhajul-im <minhajul.cpp@gmail.com>
2024-03-11 16:29:23 +0600

    home page done

--- Module-14/src/pages/HomePage.jsx
+++ Module-14/src/pages/HomePage.jsx
@@ -1,1 +1,24 @@
+import FavoriteBlogs from "../components/author/FavoriteBlogs";
+import AllBlogs from "../components/blogs/AllBlogs";
+import MostPopular from "../components/blogs/MostPopular";
+import { useAuth } from "../hooks/useAuth";
 
+const HomePage = () => {
+  const { auth } = useAuth();
+
+  return (
+    <div>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-7 gap-4">
+          <AllBlogs />
+          <div className="md:col-span-2 h-full w-full space-y-5">
+            <MostPopular />
+            {auth?.accessToken && <FavoriteBlogs />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
