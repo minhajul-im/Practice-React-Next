cfbaf41b3b10d26e5f2fb79278c1892ddf4be01f
minhajul-im <minhajul.cpp@gmail.com>
2024-03-19 06:07:12 +0600

    some property use on the div

--- Module-12/src/animation/Animations.tsx
+++ Module-12/src/animation/Animations.tsx
@@ -4,6 +4,16 @@ const Animations = () => {
   return (
     <div className="flex h-screen justify-center items-center">
       <motion.div
+        initial={{ scale: 0.5 }}
+        animate={{
+          scale: 1,
+        }}
+        transition={{ duration: 2 }}
+        className="w-52 h-52 rounded-full bg-red-500 text-yellow-500 flex justify-center items-center"
+      >
+        <h1 className="font-bold text-2xl">Hello Motions</h1>
+      </motion.div>
+      <motion.div
         animate={{
           borderRadius: ["15%", "30%", "45%", "60%", "75%", "90%", "100%"],
           scale: [0.5, 0.7, 0.9, 1, 0.9, 0.7, 0.5],
