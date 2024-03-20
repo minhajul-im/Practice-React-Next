847953c3765055cfae0375494b26ae20afdf6d05
minhajul-im <minhajul.cpp@gmail.com>
2024-03-20 07:00:58 +0600

    upadate app

--- Module-12/src/App.tsx
+++ Module-12/src/App.tsx
@@ -1,13 +1,3 @@
-import Button from "./animation/Button";
-import Div from "./animation/Div";
-import Text from "./animation/Text";
-
 export default function App() {
-  return (
-    <div>
-      <Div></Div>
-      <Button></Button>
-      <Text></Text>
-    </div>
-  );
+  return <div>hello animation</div>;
 }
