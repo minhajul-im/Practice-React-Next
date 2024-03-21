6d9747aacde5ef73435586386e0c0d86ba1776c4
minhajul-im <minhajul.cpp@gmail.com>
2024-03-21 06:04:09 +0600

    start framer motion start

--- Module-12/src/App.tsx
+++ Module-12/src/App.tsx
@@ -3,9 +3,9 @@ import Animations from "./animation/Animations";
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl text-center pt-14 text-red font-bold">
-        Hello React Portal
-      </h1>
+      {/* <h1 className="text-3xl text-center py-10 text-red font-bold">
+        Hello Framer Motions!
+      </h1> */}
       <Animations />
     </div>
   );
--- Module-12/src/animation/Animations.tsx
+++ Module-12/src/animation/Animations.tsx
@@ -1,5 +1,21 @@
+import { motion } from "framer-motion";
+
 const Animations = () => {
-  return <div>Animations</div>;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <motion.div
+        animate={{
+          borderRadius: ["15%", "30%", "45%", "60%", "75%", "90%", "100%"],
+          scale: [0.5, 0.7, 0.9, 1, 0.9, 0.7, 0.5],
+          rotate: [90, 120, 160, 180, 220, 260, 360],
+        }}
+        transition={{ duration: 2 }}
+        className="w-52 h-52 rounded-full bg-red-500 text-yellow-500 flex justify-center items-center"
+      >
+        <h1 className="font-bold text-2xl">Hello Motions</h1>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Animations;
