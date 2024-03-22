1ca9786787c3a0634d0845b436e08801e39cbd47
minhajul-im <minhajul.cpp@gmail.com>
2024-03-22 07:37:55 +0600

    start new journey next.js app

---
+++ Module-15/app/page.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-red-600 text-center">
+        Hello Next.js App
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
