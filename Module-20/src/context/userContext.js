0edde39d612b2741920d7cc302d6b5beed1af60b
Minhajul <dev.minhajul@gmail.com>
2024-04-01 17:05:46 +0600

    update module-20

--- Module-20/src/context/userContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const User = createContext(null);
-
-export default User;
--- Module-20/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import "./index.css";
-import appRoute from "./app/App";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={appRoute} />
-);
