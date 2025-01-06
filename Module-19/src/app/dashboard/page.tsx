065b303e7ef4ea01156ead210ba2ac6367881081
minhajul <dev.minhajul@gmail.com>
2025-01-06 18:09:00 +0600

    feat(site): order-page implementation

--- Module-19/src/app/dashboard/page.tsx
+++ Module-19/src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 // import { ScreenDashboardPage } from "@/modules/dashboard-page/screen-dashboard-page";
 
-import { DashboardHeader } from "@/components/dashboard-header/dashboard-header";
+// import { DashboardHeader } from "@/components/dashboard-header/dashboard-header";
 
 export default function Dashboard() {
   return (
     <main className="w-full h-full">
-      <DashboardHeader></DashboardHeader>
+      {/* <DashboardHeader></DashboardHeader> */}
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates quam
       laboriosam exercitationem distinctio provident, hic sed dignissimos atque,
       nisi sunt, odit a! Officia provident at, corporis ab ducimus quam ipsam.
--- Module-19/src/layout/dashboard-layout/dashboard-layout.tsx
+++ Module-19/src/layout/dashboard-layout/dashboard-layout.tsx
@@ -8,7 +8,7 @@ export const DashboardLayout = ({
 }) => {
   return (
     <main className="w-screen h-screen my-0 mx-auto bg-[#F0F3FA]">
-      <section className="flex flex-col-reverse md:flex-row bg-light dark:bg-night px-6 pt-6 md:pb-6 rounded-t-2xl md:rounded-2xl h-full w-full">
+      <section className="flex flex-col-reverse md:flex-row px-6 pt-6 md:pb-6 rounded-t-2xl md:rounded-2xl h-full w-full">
         <DashboardNavbar />
         <section className="flex-grow bg-white dark:bg-dark rounded-t-2xl md:rounded-2xl p-5 overflow-hidden w-full">
           <section className="flex flex-col w-full h-full box-content pr-10 overflow-y-scroll">
